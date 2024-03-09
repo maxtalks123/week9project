fc7e42cbc7586a0e514b49c0f3ab2d7026ea12e1
Max Emerson <maxemerson2013@gmail.com>
2024-03-09 16:36:44 +0000

    updated middleware

--- week9project/src/middleware.js
+++ week9project/src/middleware.js
@@ -1,7 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 });
 
 export const config = {
